f4ef5b42b31557bcbe74f5385a5e05bc040cee96
Robin Lejeune <robinlejeune@gmail.com>
2022-02-25 16:52:44 +0100

    Fetch to OMDB API done in the backend to keep the key secret

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,10 +1,12 @@
-// CODE TO ADD MOVIE TO DATABASE
-
+require('dotenv').config()
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const sqlite3 = require('sqlite3').verbose();
+const omdbApiKey = process.env.OMDB_API_KEY
 
+// Fetch the movies from the DB
 const fetchdb = (req, res) => {
 
-  let sendData = {data: []};
+  let sendData = []
 
   let db = new sqlite3.Database('db/db.moviedatabase', (err) => {
     if (err) {
@@ -17,8 +19,7 @@ const fetchdb = (req, res) => {
       if (err) {
         console.error(err.message);
       }
-      console.log(row)
-      sendData.data.push(row)
+      sendData.push(row)
 
     });
   });
@@ -27,15 +28,16 @@ const fetchdb = (req, res) => {
     if (err) {
       console.error(err.message);
     }
-    // console.log(sendData)
-    res.send(sendData)
+    res.send(JSON.stringify(sendData))
     console.log('Close the database connection.');
   });
 
 }
 
+// Add a movie to the DB
 const addmovie = (data) => {
-  const item = data.data
+  console.log(data)
+  const item = data
   const { title, year, released, runtime, genre, director, writer, actors, plot, language, country, awards, poster, imdb_rating, imdb_id, type } = item
 
   let db = new sqlite3.Database('db/db.moviedatabase');
@@ -50,20 +52,50 @@ const addmovie = (data) => {
   db.close();
 }
 
+// Delete a movie from the DB
 const deletemovie = (data) => {
   let db = new sqlite3.Database('db/db.moviedatabase');
-  db.run(`DELETE FROM movies WHERE imdbID=(?)`, [data.data.imdbID], function(err) {
+  db.run(`DELETE FROM movies WHERE imdbID=(?)`, [data.imdbID], function(err) {
       if (err) {
         return console.log(err);
       }
       // get the last insert id
-      console.log(`A row has been deleted with rowid ${data.data.id}`);
+      console.log(`A row has been deleted with rowid ${data.id}`);
   });
 
   db.close();
 }
 
+// Call the OMDB API with a movie ID
+const fetchfromomdbwithid = async (movieID, movies) => {
+  const response = await fetch(`http://www.omdbapi.com/?i=${movieID}&apikey=${omdbApiKey}`)
+  const movieInfos = await response.json()
+  console.log('Second fetch (search each movie with its ID, to retrieve all the data) done, sending data')
+  movies.push(movieInfos)
+}
+
+// Call the OMDB API with a string search
+const fetchfromomdb = async (req, res) => {
+  console.log('Fetch to OMDB API initialized')
+
+  let movies = []
+
+  const response = await fetch(`http://www.omdbapi.com/?s=${req.search}&apikey=${omdbApiKey}&`)
+  const data = await response.json()
+  
+  console.log('First fetch (search through movie titles) done')
+
+  for (const movie of data.Search) {
+    if (movie.Type === 'movie') {
+      const movieID = await movie.imdbID
+
+      await fetchfromomdbwithid(movieID, movies)  
+    }
+  }
+  res.send(JSON.stringify(movies))
+}
 
 exports.addmovie = addmovie;
 exports.deletemovie = deletemovie;
 exports.fetchdb = fetchdb;
+exports.fetchfromomdb = fetchfromomdb;
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // lib and imports
-// require('dotenv').config()
 const express = require("express");
 const app = express();
-const movie = require("./controllers/movies")
+const movie = require("./controllers/movies.js")
 
 // app setup
 app.use(express.json())
@@ -16,14 +15,18 @@ app.get('/',(req, res) => {
   res.render('movies.ejs');
 });
 
+app.get('/api/movie', movie.fetchdb)
 app.post('/api/addmovie', (req, res) => {
+  console.log('request done')
   movie.addmovie(req.body)
 })
 app.post('/api/deletemovie', (req, res) => {
   movie.deletemovie(req.body)
-  console.log("deletemovie called")
+  console.log('deletemovie called')
 })
-app.get('/api/movie', movie.fetchdb)
+app.post('/api/fetchfromomdb', (req, res) => {
+  movie.fetchfromomdb(req.body, res)
+})
 
 
 app.listen(3000, () => console.log("Server Up and running"));
